89207a87bb2812384de1882b2b417aae9a4e5fc9
Timothy Qian <12tqian@gmail.com>
2020-12-04 21:40:44 -0500

    templates

---
+++ library/contest/template_short.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define mp make_pair
+#define f first
+#define s second
+#define resz resize
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int) (x).size()
+#define all(x) begin(x), end(x)
+
+#define f1r(i, a, b) for(int i = (a); i < (b); ++i)
+#define f0r(i, a) f1r(i, 0, a)
+
+mt19937 rng((uint32_t) chrono::steady_clock::now().time_since_epoch().count());
+
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    return 0;
+}
