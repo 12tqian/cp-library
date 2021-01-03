83ad1902bc0f9dd6fd4d75531565ea661a6f601c
Timothy Qian <12tqian@gmail.com>
2021-01-03 16:21:25 -0500

    Update dynamic_convex_hull_trick.cpp

--- library/dynamic-programming/dynamic_convex_hull_trick.cpp
+++ library/dynamic-programming/dynamic_convex_hull_trick.cpp
@@ -15,7 +15,7 @@ struct ConvexHullTrick {
             return parallel(l) ? INF : 1.0 * (l.b - b) / (m - l.m);
         }
         bool operator < (const line &l) const {
-            if(l.is_query) return x < l.val;
+            if (l.is_query) return x < l.val;
             else return m < l.m;
         }
     };
@@ -25,8 +25,8 @@ struct ConvexHullTrick {
     bool bad(const line &l1, const line &l2, const line &l3) { return l1.intersect(l3) <= l1.intersect(l2); }
     bool bad(std::set<line>::iterator it) { return hull_prev(it) && hull_next(it) && bad(*prev(it), *it, *next(it)); }
     std::set<line>::iterator update(std::set<line>::iterator it) {
-        if(!hull_prev(it)) return it;
-        long double x = it -> intersect(*prev(it));
+        if (!hull_prev(it)) return it;
+        long double x = it->intersect(*prev(it));
         line tmp(*it); tmp.x = x;
         it = hull.erase(it);
         return hull.insert(it, tmp);
@@ -35,23 +35,23 @@ struct ConvexHullTrick {
         m = -m, b = -b;
         line l(m, b);
         std::set<line>::iterator it = hull.lower_bound(l);
-        if(it != hull.end() && l.parallel(*it)) {
-            if(it -> b < b) it = hull.erase(it);
+        if (it != hull.end() && l.parallel(*it)) {
+            if(it->b < b) it = hull.erase(it);
             else return;
         }
         it = hull.insert(it, l);
-        if(bad(it)) return (void) hull.erase(it);
-        while(hull_prev(it) && bad(prev(it))) hull.erase(prev(it));
-        while(hull_next(it) && bad(next(it))) hull.erase(next(it));
+        if (bad(it)) return (void) hull.erase(it);
+        while (hull_prev(it) && bad(prev(it))) hull.erase(prev(it));
+        while (hull_next(it) && bad(next(it))) hull.erase(next(it));
         it = update(it);
-        if(hull_prev(it)) update(prev(it));
-        if(hull_next(it)) update(next(it));
+        if (hull_prev(it)) update(prev(it));
+        if (hull_next(it)) update(next(it));
     }
     long long query(long long x) const {
-        if(hull.empty()) return INF;
+        if (hull.empty()) return INF;
         line q; q.val = x, q.is_query = 1;
         std::set<line>::iterator it = --hull.lower_bound(q);
-        return -(it -> eval(x));
+        return -(it->eval(x));
     }
 };
 
