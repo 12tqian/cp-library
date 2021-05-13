950565878f727fbedc7d23fd8e6010bcf2636bea
Timothy Qian <12tqian@gmail.com>
2021-05-13 11:48:14 -0400

    templates

--- library/contest/template_full.cpp
+++ library/contest/template_full.cpp
@@ -1,6 +1,103 @@
-#include <bits/stdc++.h>
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+typedef long long ll;
+typedef long double ld;
+typedef double db;
+typedef string str;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<db, db> pd;
+
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<ll> vl;
+typedef vector<db> vd;
+typedef vector<str> vs;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<pd> vpd;
+
+#define mp make_pair
+#define f first
+#define s second
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define resz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+
+#define f1r(i, a, b) for (int i = (a); i < (b); ++i)
+#define f0r(i, a) f1r(i, 0, a)
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i,0,a)
+#define ROF(i, a, b) for (int i = (b) - 1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define each(a, x) for (auto& a : x)
+
+mt19937 rng((uint32_t) chrono::steady_clock::now().time_since_epoch().count());
+
+template <class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template <class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+template <class T> using V = vector<T>;
+template <class T> using VV = V<V<T>>;
+template <class T> using VVV = V<V<V<T>>>;
+template <class T> using VVVV = V<V<V<V<T>>>>;
+
+#ifdef LOCAL
+#define dbg(...) debug(#__VA_ARGS__, __VA_ARGS__);
+#else
+#define dbg(...) 17;
+#endif
+
+template <typename T, typename S> ostream& operator << (ostream &os, const pair<T, S> &p) { return os << "(" << p.first << ", " << p.second << ")"; }
+template <typename C, typename T = decay<decltype(*begin(declval<C>()))>, typename enable_if<!is_same<C, string>::value>::type* = nullptr>
+ostream& operator << (ostream &os, const C &c) { bool f = true; os << "{"; for (const auto &x : c) { if (!f) os << ", "; f = false; os << x; } return os << "}"; }
+template <typename T> void debug(string s, T x) { cerr << s << " = " << x << "\n"; }
+template <typename T, typename... Args> void debug(string s, T x, Args... args) { cerr << s.substr(0, s.find(',')) << " = " << x << " | "; debug(s.substr(s.find(',') + 2), args...); }
+
+constexpr int pct(int x) { return __builtin_popcount(x); }
+constexpr int bits(int x) { return 31 - __builtin_clz(x); } // floor(log2(x))
 
 inline namespace Helpers {
     //////////// is_iterable
@@ -147,4 +244,56 @@ inline namespace FileIO {
         // ex. try to read letter into int
         if (!s.empty()) set_in(s + ".in"), set_out(s + ".out"); // for old USACO
     }
+}
+
+const int MOD = 1e9 + 7; // 998244353;
+const ld PI = acos((ld) -1);
+
+typedef std::decay <decltype(MOD)>::type mod_t;
+struct mi {
+    mod_t val;
+    explicit operator mod_t() const { return val; }
+    mi() { val = 0; }
+    mi(const long long& v) {
+        val = (-MOD <= v && v <= MOD) ? v : v % MOD;
+        if (val < 0) val += MOD; }
+    friend std::istream& operator >> (std::istream& in, mi& a) { 
+        long long x; std::cin >> x; a = mi(x); return in; }
+    friend std::ostream& operator << (std::ostream& os, const mi& a) { return os << a.val; }
+    friend void pr(const mi& a) { pr(a.val); }
+    friend void re(mi& a) { long long x; cin >> x; a = mi(x); }
+    friend bool operator == (const mi& a, const mi& b) { return a.val == b.val; }
+    friend bool operator != (const mi& a, const mi& b) { return !(a == b); }    
+    friend bool operator < (const mi& a, const mi& b) { return a.val < b.val; }
+    friend bool operator > (const mi& a, const mi& b) { return a.val > b.val; }
+    friend bool operator <= (const mi& a, const mi& b) { return a.val <= b.val; }
+    friend bool operator >= (const mi& a, const mi& b) { return a.val >= b.val; }
+    mi operator - () const { return mi(-val); }
+    mi& operator += (const mi& m) {
+        if ((val += m.val) >= MOD) val -= MOD;
+        return *this; }
+    mi& operator -= (const mi& m) {
+        if ((val -= m.val) < 0) val += MOD;
+        return *this; }
+    mi& operator *= (const mi& m) { val = (long long) val * m.val % MOD;
+        return *this; }
+    friend mi pow(mi a, long long p) {
+        mi ans = 1; assert(p >= 0);
+        for (; p; p /= 2, a *= a) if (p & 1) ans *= a;
+        return ans; }
+    friend mi inv(const mi& a) { assert(a != 0); return pow(a, MOD - 2); }
+    mi& operator /= (const mi& m) { return (*this) *= inv(m); }
+    friend mi operator + (mi a, const mi& b) { return a += b; }
+    friend mi operator - (mi a, const mi& b) { return a -= b; }
+    friend mi operator * (mi a, const mi& b) { return a *= b; }
+    friend mi operator / (mi a, const mi& b) { return a /= b; }
+};
+
+typedef pair<mi, mi> pmi;
+typedef vector<mi> vmi;
+typedef vector<pmi> vpmi;
+
+int main() {
+    setIO("");
+    return 0;
 }
