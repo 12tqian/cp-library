3b7bc55c0851fe03eca661abbdb9a9270e1364f5
Timothy Qian <12tqian@gmail.com>
2021-07-03 15:25:20 -0400

    Create range_vector.cpp

---
+++ library/misc/range_vector.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+
+template <class T> struct range_vector : public std::vector<T> {
+	int min_index;
+
+	range_vector() : min_index(0) {}
+
+	range_vector(int _min_index, int _max_index, T&& values = T())
+		: std::vector<T>(_max_index - _min_index + 1, values), min_index(_min_index) {
+		assert(_min_index <= _max_index + 1);
+	}
+	
+	range_vector(int _max_index, T&& values = T())
+		: std::vector<T>(_max_index + 1, values), min_index(0) {
+		assert(0 <= _max_index + 1);
+	}
+	
+	T& operator[](int i) { std::vector<T>& self = *this; return self[i - min_index]; }
+};
