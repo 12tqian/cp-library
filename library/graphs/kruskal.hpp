cc906c448504893def3b5312ac3c2c24420f95e4
Timothy Qian <12tqian@gmail.com>
2021-07-25 00:45:08 -0400

    Update kruskal.hpp

--- library/graphs/kruskal.hpp
+++ library/graphs/kruskal.hpp
@@ -1,31 +1,6 @@
 #pragma once
 
-struct DSU {
-	std::vector<int> e;
-	void init(int n) {
-		e = std::vector<int>(n, -1);
-	}
-
-	int get(int x) {
-		return e[x] < 0 ? x : e[x] = get(e[x]);
-	}
-
-	bool same_set(int a, int b) {
-		return get(a) == get(b);
-	}
-
-	int size(int x) {
-		return -e[get(x)];
-	}
-
-	bool unite(int x, int y) {
-		x = get(x), y = get(y);
-		if (x == y) return false;
-		if (e[x] > e[y]) std::swap(x, y);
-		e[x] += e[y]; e[y] = x;
-		return true;
-	}
-};
+#include "dsu.hpp"
 
 // weight, start, end
 long long kruskal(std::vector<std::array<int, 3>> edge) {
