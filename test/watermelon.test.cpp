f6e92de0f17d842933bbacd5e529abb8c3e095c8
Timothy Qian <12tqian@gmail.com>
2021-07-25 20:57:35 -0400

    Update watermelon.test.cpp

--- test/watermelon.test.cpp
+++ test/watermelon.test.cpp
@@ -1,16 +1,16 @@
-	#define PROBLEM "https://codeforces.com/contest/4/problem/A"
+#define PROBLEM "https://codeforces.com/contest/4/problem/A"
 
-	#include "../library/contest/template-minimal.hpp"
+#include "../library/contest/template-minimal.hpp"
 
-	int main() {
-		ios::sync_with_stdio(false);
-		cin.tie(nullptr);
-		int n;
-		cin >> n;
-		if (n > 2 && n % 2 == 0) {
-			cout << "YES" << '\n';
-		} else {
-			cout << "NO" << '\n';
-		}
-		return 0; 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int n;
+	cin >> n;
+	if (n > 2 && n % 2 == 0) {
+		cout << "YES" << '\n';
+	} else {
+		cout << "NO" << '\n';
 	}
+	return 0; 
+}
