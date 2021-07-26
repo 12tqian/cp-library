8379cda02f7ba4aeba88159ce53d8d852e90c334
Timothy Qian <12tqian@gmail.com>
2021-07-25 21:09:56 -0400

    Update watermelon.test.cpp

--- test/watermelon.test.cpp
+++ test/watermelon.test.cpp
@@ -5,12 +5,15 @@
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
+	
 	int n;
 	cin >> n;
+
 	if (n > 2 && n % 2 == 0) {
 		cout << "YES" << '\n';
 	} else {
 		cout << "NO" << '\n';
 	}
+	
 	return 0; 
 }
