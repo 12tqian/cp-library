9582bdab5186f23b3d88a47ffce7ee5cdc8b3937
Timothy Qian <12tqian@gmail.com>
2021-07-29 01:25:37 -0400

    fix

--- verify/yosupo/yosupo-point_add_range_sum-range-set-segment-tree.test.cpp
+++ verify/yosupo/yosupo-point_add_range_sum-range-set-segment-tree.test.cpp
@@ -2,34 +2,30 @@
 
 #include "../../library/contest/template-minimal.hpp"
 #include "../../library/data-structures/1d-range-queries/range-set-segment-tree.hpp"
-#include "../../library/misc/fast-io.hpp"
-
-FastIO::Scanner sc(stdin);
-FastIO::Printer pr(stdout);
 
 int main() {
 	using namespace std;
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	int n, q;
-	sc.read(n, q);
+	cin >> n >> q;
 	vector<long long> a(n);
 	RangeSetSeg<long long> seg; 
 	seg.init(n);
 	for (int i = 0; i < n; i++) 
-		sc.read(a[i]), seg.range_set(i, i, a[i]);
+		cin >> a[i], seg.range_set(i, i, a[i]);
 	while (q--) {
 		int t;
-		sc.read(t);
+		cin >> t;
 		if (t == 0) {
 			int p, x;
-			sc.read(p, x);
+			cin >> p >> x;
 			seg.range_set(p, p, x + seg.qsum(p, p));
 		} else {
 			int l, r;
-			sc.read(l, r);
+			cin >> l >> r;
 			--r;
-			pr.writeln(seg.qsum(l, r));
+			cout << seg.qsum(l, r) << '\n';
 		}
 	}
 	return 0;
