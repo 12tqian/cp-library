6a2ea295b624b346ed5dca950571f9e1a64f5451
Timothy Qian <12tqian@gmail.com>
2021-07-30 00:35:28 -0400

    Create yosupo-inverse_matrix.test.cpp

---
+++ verify/yosupo/yosupo-inverse_matrix.test.cpp
@@ -0,0 +1,35 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/inverse_matrix"
+
+#include "../../library/contest/template-minimal.hpp"
+#include "../../library/numerical/matrix.hpp"
+#include "../../library/numerical/mod-int2.hpp"
+
+using mi = Mint<998244353, 5>;
+
+int main() {
+	using namespace MatrixOperations;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int n;
+	cin >> n;
+	auto a = make_matrix<mi>(n, n);
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			cin >> a[i][j];
+		}
+	}
+	auto tmp = a;
+	mi det = gauss(tmp).first;
+	if (det == 0) {
+		cout << -1 << '\n';
+	} else {
+		auto ans = inv(a);
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				cout << ans[i][j] << ' ';
+			}
+			cout << '\n';
+		}
+	}
+	return 0;
+}
