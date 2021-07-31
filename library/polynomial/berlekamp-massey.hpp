9e512cc13edaecf5804837af18df06c0c1b2e3c2
Timothy Qian <12tqian@gmail.com>
2021-07-31 02:19:05 -0400

    fix

--- library/polynomial/berlekamp-massey.hpp
+++ library/polynomial/berlekamp-massey.hpp
@@ -2,24 +2,24 @@
 
 #include "polynomial.hpp"
 
-template <class Mint> Poly<Mint> berlekamp_massey(const Poly<Mint>& s) {
+template <class D> Poly<D> berlekamp_massey(const Poly<D>& s) {
 	int n = int(s.size());
-	std::vector<Mint> b = {Mint(-1)}, c = {Mint(-1)};
-	Mint y = Mint(1);
+	std::vector<D> b = {D(-1)}, c = {D(-1)};
+	D y = D(1);
 	for (int ed = 1; ed <= n; ed++) {
 		int l = int(c.size()), m = int(b.size());
-		Mint x = 0;
+		D x = 0;
 		for (int i = 0; i < l; i++) {
 			x += c[i] * s[ed - l + i];
 		}
 		b.push_back(0);
 		m++;
 		if (!x) continue;
-		Mint freq = x / y;
+		D freq = x / y;
 		if (l < m) {
 			// use b
 			auto tmp = c;
-			c.insert(begin(c), m - l, Mint(0));
+			c.insert(begin(c), m - l, D(0));
 			for (int i = 0; i < m; i++) {
 				c[m - 1 - i] -= freq * b[m - 1 - i];
 			}
