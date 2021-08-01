d178af67513eef553d13694aabd7ab8a163f07c5
Timothy Qian <12tqian@gmail.com>
2021-07-31 22:47:04 -0400

    Update matrix.hpp

--- library/numerical/matrix.hpp
+++ library/numerical/matrix.hpp
@@ -171,7 +171,7 @@ template <class D> std::vector<std::vector<D>> solve_linear(Matrix<D> a, std::ve
     }
 	auto zero = [&](const D& x) {
 		return EPS == D(-1) ? x != 0 : -EPS < x && x < EPS;
-	}
+	};
     for (int y = r; y < h; y++) {
         if (!zero(b[y])) return {};
     }
