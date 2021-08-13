4fda8f180e75bf359a15c491d5ea0873bad45f7d
Timothy Qian <12tqian@gmail.com>
2021-08-13 01:29:17 -0400

    docs

--- library/numerical/xor-basis.hpp
+++ library/numerical/xor-basis.hpp
@@ -1,11 +1,6 @@
 #pragma once
 
-/**
- * full_reduce() does complete Gaussian reduction
- * reduce() just does the things that are necessary, should be sufficient
- */
-
-void full_reduce(std::vector<int> &b) {
+void full_reduce(std::vector<int>& b) {
 	int n = (int)b.size();
 	for (int i = n - 1; i >= 0; i--) {
 		for (int j = i - 1; j >= 0; j--) {
@@ -14,14 +9,14 @@ void full_reduce(std::vector<int> &b) {
 	}
 }
 
-template <class T> T reduce(std::vector<T> &b, T x) {
-	for (auto &t : b) {
+template <class T> T reduce(std::vector<T>& b, T x) {
+	for (auto& t : b) {
 		x = std::min(x, x ^ t);
 	}
 	return x;
 }
 
-template <class T> bool add(std::vector<T> &b, T x) {
+template <class T> bool add(std::vector<T>& b, T x) {
 	if (!(x = reduce(b, x))) return false;
 	int ind = 0;
 	while (ind < (int)b.size() && b[ind] > x) ind++;
---
+++ library/numerical/xor-basis.md
@@ -0,0 +1,14 @@
+---
+title: XOR Basis
+documentation_of: xor-basis.hpp
+---
+
+## XOR Basis
+
+### Functions
+- `full_reduce(b)`: Full Gaussian elimination on everything. 
+- `reduce(b)`: Just reduces enough to work. 
+- `add(b, x)`: Adds $x$ to a basis $b$. 
+
+### Resources
+- [Benq](https://github.com/bqi343/USACO/blob/4aa96cd195a770c3a7f8977441020036d84b4f24/Implementations/content/numerical/Matrix%20(11.3)/XorBasis.h)
