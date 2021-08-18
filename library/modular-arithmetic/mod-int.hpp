8b5464c8413ef16487def0137eeb13419221570f
Timothy Qian <12tqian@gmail.com>
2021-08-18 19:19:26 -0400

    Update mod-int.hpp

--- library/modular-arithmetic/mod-int.hpp
+++ library/modular-arithmetic/mod-int.hpp
@@ -4,30 +4,30 @@ const int MOD = 1e9 + 7; // 998244353
 
 typedef std::decay<decltype(MOD)>::type mod_t; 
 struct mi {
-	mod_t val;
-	explicit operator mod_t() const { return val; }
-	explicit operator bool() const { return val != 0; }
-	mi() { val = 0; }
+	mod_t v;
+	explicit operator mod_t() const { return v; }
+	explicit operator bool() const { return v != 0; }
+	mi() { v = 0; }
 	mi(const long long& v) {
-		val = (-MOD <= v && v < MOD) ? v : v % MOD;
-		if (val < 0) val += MOD; }
+		v = (-MOD <= v && v < MOD) ? v : v % MOD;
+		if (v < 0) v += MOD; }
 	friend std::istream& operator>>(std::istream& in, mi& a) { 
 		long long x; std::cin >> x; a = mi(x); return in; }
-	friend std::ostream& operator<<(std::ostream& os, const mi& a) { return os << a.val; }
-	friend bool operator==(const mi& a, const mi& b) { return a.val == b.val; }
+	friend std::ostream& operator<<(std::ostream& os, const mi& a) { return os << a.v; }
+	friend bool operator==(const mi& a, const mi& b) { return a.v == b.v; }
 	friend bool operator!=(const mi& a, const mi& b) { return !(a == b); }    
-	friend bool operator<(const mi& a, const mi& b) { return a.val < b.val; }
-	friend bool operator>(const mi& a, const mi& b) { return a.val > b.val; }
-	friend bool operator<=(const mi& a, const mi& b) { return a.val <= b.val; }
-	friend bool operator>=(const mi& a, const mi& b) { return a.val >= b.val; }
-	mi operator-() const { return mi(-val); }
+	friend bool operator<(const mi& a, const mi& b) { return a.v < b.v; }
+	friend bool operator>(const mi& a, const mi& b) { return a.v > b.v; }
+	friend bool operator<=(const mi& a, const mi& b) { return a.v <= b.v; }
+	friend bool operator>=(const mi& a, const mi& b) { return a.v >= b.v; }
+	mi operator-() const { return mi(-v); }
 	mi& operator+=(const mi& m) {
-		if ((val += m.val) >= MOD) val -= MOD;
+		if ((v += m.v) >= MOD) v -= MOD;
 		return *this; }
 	mi& operator-=(const mi& m) {
-		if ((val -= m.val) < 0) val += MOD;
+		if ((v -= m.v) < 0) v += MOD;
 		return *this; }
-	mi& operator*=(const mi& m) { val = (long long)val * m.val % MOD;
+	mi& operator*=(const mi& m) { v = (long long)v * m.v % MOD;
 		return *this; }
 	friend mi pow(mi a, long long p) {
 		mi ans = 1; assert(p >= 0);
