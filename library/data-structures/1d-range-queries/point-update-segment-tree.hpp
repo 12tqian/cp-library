48720adc998016fcf7a406497bd619553827dea2
Timothy Qian <12tqian@gmail.com>
2023-01-08 15:53:16 -0500

    constructors

--- library/data-structures/1d-range-queries/point-update-segment-tree.hpp
+++ library/data-structures/1d-range-queries/point-update-segment-tree.hpp
@@ -1,40 +1,40 @@
 #pragma once
 
-template <class T> struct SegmentTree {
-	const T ID = 0;
+template <class T>
+struct SegmentTree {
+  const T ID = 0;
 
-	T comb(T a, T b) {
-		return a + b;
-	}
+  T comb(T a, T b) { return a + b; }
 
-	int n;
-	std::vector<T> seg;
+  int n;
+  std::vector<T> seg;
 
-	void init(int _n) {
-		n = _n;
-		seg.assign(2 * n, ID);
-	}
+  SegmentTree() = default;
+  SegmentTree(int n) { init(n); }
 
-	void pull(int p) {
-		seg[p] = comb(seg[2 * p], seg[2 * p + 1]);
-	}
+  void init(int _n) {
+    n = _n;
+    seg.assign(2 * n, ID);
+  }
 
-	void upd(int p, T val) {
-		seg[p += n] = val;
-		for (p /= 2; p; p /= 2) pull(p);
-	}
+  void pull(int p) { seg[p] = comb(seg[2 * p], seg[2 * p + 1]); }
 
-	void add(int p, T val) {
-		seg[p += n] += val;
-		for (p /= 2; p; p /= 2) pull(p);
-	}
+  void upd(int p, T val) {
+    seg[p += n] = val;
+    for (p /= 2; p; p /= 2) pull(p);
+  }
 
-	T query(int l, int r) {
-		T ra = ID, rb = ID;
-		for (l += n, r += n + 1; l < r; l /= 2, r /= 2) {
-			if (l & 1) ra = comb(ra, seg[l++]);
-			if (r & 1) rb = comb(seg[--r], rb);
-		}
-		return comb(ra, rb);
-	}
+  void add(int p, T val) {
+    seg[p += n] += val;
+    for (p /= 2; p; p /= 2) pull(p);
+  }
+
+  T query(int l, int r) {
+    T ra = ID, rb = ID;
+    for (l += n, r += n + 1; l < r; l /= 2, r /= 2) {
+      if (l & 1) ra = comb(ra, seg[l++]);
+      if (r & 1) rb = comb(seg[--r], rb);
+    }
+    return comb(ra, rb);
+  }
 };
