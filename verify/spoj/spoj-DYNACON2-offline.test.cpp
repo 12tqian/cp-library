8f4be988efa6269b02aeb9462aaf48074bc6c085
Timothy Qian <12tqian@gmail.com>
2023-01-17 11:06:24 -0500

    format

--- verify/spoj/spoj-DYNACON2-offline.test.cpp
+++ verify/spoj/spoj-DYNACON2-offline.test.cpp
@@ -4,40 +4,40 @@
 #include "../../library/graphs/offline-dynamic-connectivity.hpp"
 
 int main() {
-    using namespace std;
-    int n, m; cin >> n >> m;
-    OfflineDynamicConnectivity O;
-    O.init(n, m + 1);
-    set<array<int, 3>> edges;
-    for (int i = 0; i < m; i++) {
-        string s; cin >> s;
-        if (s == "add") {
-            int u, v; 
-            cin >> u >> v;
-            u--, v--;
-            if (u > v) swap(u, v);
-            edges.insert({u, v, i});
-        } else if (s == "rem") {
-            int u, v; 
-            cin >> u >> v; 
-            u--, v--;
-            if (u > v) swap(u, v);
-            auto e = *edges.lower_bound({u, v, -1});
-            O.upd(e[2], i, {u, v});
-            edges.erase(e);
-        } else {
-            int u, v; 
-            cin >> u >> v;
-            u--, v--;
-            O.add_query(i, u, v);
-        }
-    }
-    for (auto e : edges) {
-        O.upd(e[2], m, {e[0], e[1]});
-    }
-    auto ans = O.solve();
-    for (auto x : ans) {
-        cout << (x ? "YES" : "NO") << '\n';
-    }
-    return 0;
+	using namespace std;
+	int n, m; cin >> n >> m;
+	OfflineDynamicConnectivity O;
+	O.init(n, m + 1);
+	set<array<int, 3>> edges;
+	for (int i = 0; i < m; i++) {
+		string s; cin >> s;
+		if (s == "add") {
+			int u, v; 
+			cin >> u >> v;
+			u--, v--;
+			if (u > v) swap(u, v);
+			edges.insert({u, v, i});
+		} else if (s == "rem") {
+			int u, v; 
+			cin >> u >> v; 
+			u--, v--;
+			if (u > v) swap(u, v);
+			auto e = *edges.lower_bound({u, v, -1});
+			O.upd(e[2], i, {u, v});
+			edges.erase(e);
+		} else {
+			int u, v; 
+			cin >> u >> v;
+			u--, v--;
+			O.add_query(i, u, v);
+		}
+	}
+	for (auto e : edges) {
+		O.upd(e[2], m, {e[0], e[1]});
+	}
+	auto ans = O.solve();
+	for (auto x : ans) {
+		cout << (x ? "YES" : "NO") << '\n';
+	}
+	return 0;
 }
